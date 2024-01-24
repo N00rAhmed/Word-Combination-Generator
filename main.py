130aea7da7fc6a7678f9c3963cbe4647da0a0957
N00rAhmed <tronn232003@gmail.com>
2024-01-24 17:11:54 +0000

    first commit to work maker

---
+++ main.py
@@ -0,0 +1,24 @@
+# main/main.py
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/nav")
+def navigation():
+    return render_template('nav.html')
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/wordmaker')
+def wordMaker():
+    return render_template('wordMaker.html')
