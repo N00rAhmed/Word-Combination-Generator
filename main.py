cd79da298a645e85bc9d2313a318d0a6cad492f2
N00rAhmed <tronn232003@gmail.com>
2024-02-09 17:39:49 +0000

    currently made it ouput on ui the user input via button click

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main/main.py
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import json
 
@@ -21,21 +21,29 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/wordmaker', methods=['GET'])
+@app.route('/wordmaker', methods=['GET', 'POST'])
 def wordMaker():
     
-    api_url = "https://api.dictionaryapi.dev/api/v2/entries/en/mango"
+    letterinput = ""
 
-    try:
-        response = requests.get(api_url)
-        response_data = json.loads(response.text)
-        formatted_data = json.dumps(response_data, indent=4)
-        return render_template('wordMaker.html', data=formatted_data)
+    if request.method == 'POST':
+        letterinput = request.form['letterinput']
+    return render_template('wordMaker.html', letterinput=letterinput)
 
-    except Exception as e:
-        return render_template('wordMaker.html', error=str(e))
+
+    # api_url = "https://api.dictionaryapi.dev/api/v2/entries/en/mango"
+
+    # try:
+    #     response = requests.get(api_url)
+    #     response_data = json.loads(response.text)
+    #     formatted_data = json.dumps(response_data, indent=4)
+    #     return render_template('wordMaker.html', data=formatted_data)
+
+    # except Exception as e:
+    #     return render_template('wordMaker.html', letterinput=letterinput, error=str(e))
 
 
 
 if  __name__ == "__main__":
     app.run(debug=True)
+
