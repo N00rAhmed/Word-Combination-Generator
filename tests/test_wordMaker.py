7e620b5af02d855a6195be24b42391a43ac3c304
N00rAhmed <tronn232003@gmail.com>
2024-04-02 16:33:25 +0100

    small change

--- tests/test_wordMaker.py
+++ tests/test_wordMaker.py
@@ -1,18 +1,12 @@
-import unittest
-from flask import Flask, request, json
-from collections import Counter
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import pytest
+from flask import Flask
+from flask_testing import TestCase
+from main import wordMaker # Import your Flask app
 
-from main import wordMaker
+class TestWordMaker(TestCase):
+    def create_app(self):
+        wordMaker.config['TESTING'] = True
+        return wordMaker
 
-class WordMakerTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = Flask(__name__)
-        self.app.config['TESTING'] = True
-        self.client = self.app.test_client()
 
-    def test_word_maker_with_valid_input(self):
-        response = self.client.post('/wordmaker', data={'letterinput': 'etad'})
-        self.assertEqual(response.status_code, 200)
+
