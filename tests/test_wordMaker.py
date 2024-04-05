bfe5368cee1f710270b5d8e692b6a01a495dcd48
N00rAhmed <tronn232003@gmail.com>
2024-04-05 17:13:59 +0100

    removed comments

--- tests/test_wordMaker.py
+++ tests/test_wordMaker.py
@@ -1,12 +1,9 @@
 import pytest
 from flask import Flask
 from flask_testing import TestCase
-from main import wordMaker # Import your Flask app
+from main import wordMaker
 
 class TestWordMaker(TestCase):
     def create_app(self):
         wordMaker.config['TESTING'] = True
         return wordMaker
-
-
-
